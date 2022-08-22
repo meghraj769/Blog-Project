2c0de12281d8416c9e3c6e30f074c578ca0e1602
meghraj769 <meghrajgupta9731@gmail.com>
2022-08-22 15:15:54 +0530

    added required files for heroku

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-hwo6+*er*&2sl8l7(34hyt72@k(^k=t!lah(+tv@jsx6p^-uvs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1:8000', '127.0.0.1' ]
+ALLOWED_HOSTS = ['127.0.0.1:8000', '127.0.0.1', 'https://blog-site-meghraj.herokuapp.com/']
 
 
 # Application definition
